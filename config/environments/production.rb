c553dbde41af3f67b70985c8f1d6bc07c3fd5f6f
uselesssas <sas.clicking@mail.ru>
2022-07-15 01:16:03 +0300

    setting devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # devise
+  config.action_mailer.default_url_options = { :host => 'https://delicat-fromage-52007.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
